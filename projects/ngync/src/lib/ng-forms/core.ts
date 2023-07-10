6ea233e18cf50de6f7f646490f7b6eb5f7a0d9e9
Oleksii Shepel <oleksii.shepel@outlook.com>
2023-07-10 18:43:42 +0300

    corrections

--- projects/ngync/src/lib/ng-forms/core.ts
+++ projects/ngync/src/lib/ng-forms/core.ts
@@ -41,6 +41,7 @@ import {
   NGYNC_CONFIG_DEFAULT,
   NGYNC_CONFIG_TOKEN,
   deepEqual,
+  getValue,
   selectValue,
   setValue
 } from '.';
@@ -58,7 +59,7 @@ import {
   UpdateStatus
 } from './actions';
 import { Queue } from './queue';
-import { actionQueues } from './reducers';
+import { actionQueues, selectDirty } from './reducers';
 
 
 export interface NgyncConfig {
@@ -100,6 +101,13 @@ export class SyncDirective implements OnInit, OnDestroy, AfterContentInit {
   }
 
   inputCallback = (control: NgControl) => (value : any) => {
+    if(control.value !== value && control.control) {
+      control.control.setValue(value, { emitEvent: false });
+
+      const state = this.submittedState ?? this.initialState;
+      const savedState = control.path ? getValue(state, control.path.join('.')) : undefined;
+      !(savedState === control.value) ? control.control.markAsDirty() : control.control.markAsPristine();
+    }
     if(this.updateOn === 'change' && control.path) {
       this.store.dispatch(UpdateField({ path: this.slice, property: control.path.join('.'), value: value }));
     }
@@ -156,15 +164,10 @@ export class SyncDirective implements OnInit, OnDestroy, AfterContentInit {
       mergeMap((value) => from(this.initialized$).pipe(filter(value => value), take(1), map(() => value))),
       tap(() => {
 
-        if(this.updateOn === 'submit') {
-          this.store.dispatch(UpdateForm({path: this.slice, value: this.formValue}));
-        }
-
         this.submittedState = this.formValue;
 
-        if (this.dir.form.dirty) {
-          this.dir.form.markAsPristine();
-          this.cdr.markForCheck();
+        if(this.updateOn === 'submit') {
+          this.store.dispatch(UpdateForm({path: this.slice, value: this.formValue}));
         }
       }),
       tap(() => ( this.store.dispatch(AutoSubmit({ path: this.slice })))),
@@ -175,22 +178,20 @@ export class SyncDirective implements OnInit, OnDestroy, AfterContentInit {
       filter((action: any) => action && action.path === this.slice && action.type === FormActions.UpdateField),
       sampleTime(this.debounceTime),
       mergeMap((value) => from(this.initialized$).pipe(filter(value => value), take(1), map(() => value))),
-      tap((action: any) => {
+      mergeMap((value) => this.store.select(selectDirty(this.slice)).pipe(take(1), map((dirty) => ({action: value, dirty: dirty})))),
+      tap(({action, dirty}) => {
         const path = action.property.split('.');
-        const control = path.reduce((acc: any, key: string, index: number) => acc[key], this.dir.form.controls);
+        const control = path.reduce((acc: any, key: string, index: number) => acc.controls[key], this.dir.form);
 
-        if(action.value !== control.value) {
-          control.setValue(action.value, { emitEvent: false });
-          const dirty = !deepEqual(this.formValue, this.submittedState ?? this.initialState);
+        const notEqual = !deepEqual(this.formValue, this.submittedState ?? this.initialState);
 
-          if(this.dir.form.dirty !== dirty) {
-            dirty ? this.dir.form.markAsDirty() : this.dir.form.markAsPristine();
-            this.store.dispatch(UpdateDirty({ path: this.slice, dirty: this.dir.form.dirty }));
-          }
-
-          control.updateValueAndValidity();
-          this.cdr.markForCheck();
+        if(dirty !== notEqual) {
+          notEqual ? this.dir.form.markAsDirty() : this.dir.form.markAsPristine();
+          this.store.dispatch(UpdateDirty({ path: this.slice, dirty: notEqual }));
         }
+
+        control.updateValueAndValidity();
+        this.cdr.markForCheck();
       }),
       takeWhile(() => !this.destoyed)
     );
@@ -200,13 +201,13 @@ export class SyncDirective implements OnInit, OnDestroy, AfterContentInit {
       filter((action: any) => (!this.enableQueue || action.deferred)),
       tap((action) => {
 
-        this.dir.form.patchValue(action?.value, {emitEvent: false});
+        this.dir.form.patchValue(action.value, {emitEvent: false});
 
         const initialized = this.initialized$.value;
-        const dirty = !initialized ? false : !deepEqual(action?.value, this.submittedState ?? this.initialState);
+        const dirty = !initialized ? false : !deepEqual(action.value, this.submittedState ?? this.initialState);
 
         if(!initialized) {
-          this.initialState = action?.value;
+          this.initialState = action.value;
           this.initialized$.next(true);
         }
 
--- projects/ngync/src/lib/ng-forms/module.ts
+++ projects/ngync/src/lib/ng-forms/module.ts
@@ -5,7 +5,7 @@ import { StoreModule } from '@ngrx/store';
 import { NgyncConfig, SyncDirective } from './core';
 
 export const NGYNC_CONFIG_TOKEN = new InjectionToken<string>('NGYNC_CONFIG_TOKEN');
-export const NGYNC_CONFIG_DEFAULT: Omit<NgyncConfig, 'slice'> = { debounceTime: 100, updateOn: 'change', enableQueue: true };
+export const NGYNC_CONFIG_DEFAULT: Omit<NgyncConfig, 'slice'> = { debounceTime: 500, updateOn: 'change', enableQueue: true };
 
 @NgModule({
   imports: [FormsModule, StoreModule],
