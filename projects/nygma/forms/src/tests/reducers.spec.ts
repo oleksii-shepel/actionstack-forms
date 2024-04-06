b898ab1218eee8517d5897ca84e71ac50d7e66f7
Oleksii Shepel <113843497+oleksii-shepel@users.noreply.github.com>
2024-04-06 16:04:07 +0300

    Update reducers.spec.ts

--- projects/nygma/forms/src/tests/reducers.spec.ts
+++ projects/nygma/forms/src/tests/reducers.spec.ts
@@ -22,23 +22,23 @@ describe('reducers', () => {
       }
     };
 
-    const f = await forms(initialState)((state: any, action: any) => { return state; });
+    const f = forms(initialState)((state: any, action: any) => { return state; });
     let expected = {} as any;
 
-    let newState = await f(initialState, autoInit({path: "slice.form", value: model}));
+    let newState = f(initialState, autoInit({path: "slice.form", value: model}));
     expected = { slice: { form: { ...model, __form: true }} }; initialState = newState;
     expect(newState).toEqual(expected);
 
-    newState = await f(initialState, updateForm({path: "slice.form", value: model}));
+    newState = f(initialState, updateForm({path: "slice.form", value: model}));
     expected = { slice: { form: { ...model, __form: true }} };
     expect(newState).toEqual(expected);
 
-    newState = await f(initialState, updateControl({path: "slice.form", property: "email", value: model.email}));
+    newState = f(initialState, updateControl({path: "slice.form", property: "email", value: model.email}));
     expected = { slice: { form: { ...model, __form: true }} }
     expect(newState).toEqual(expected);
 
-    newState = await f(initialState, autoSubmit({path: "slice.form"}));
+    newState = f(initialState, autoSubmit({path: "slice.form"}));
 
-    newState = await f(initialState, formDestroyed({path: "slice.form", value: initialState}));
+    newState = f(initialState, formDestroyed({path: "slice.form", value: initialState}));
   });
 });
