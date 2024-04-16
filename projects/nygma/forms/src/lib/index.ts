277ad4046c095af2706a75d139616df5e1cb5bf2
Oleksii Shepel <oleksii.shepel@outlook.com>
2024-04-16 10:49:41 +0300

    corrections in logger

--- projects/nygma/forms/src/lib/index.ts
+++ projects/nygma/forms/src/lib/index.ts
@@ -1,8 +1,9 @@
 export * from './actions';
 export * from './builder';
 export * from './core';
+export * from './logger';
 export * from './module';
 export * from './reducers';
-export * from './utils';
 export * from './timers';
+export * from './utils';
 
